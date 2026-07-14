e99f725fe0f9ee83025c98f7f47fadd15a6061c7
Allansrn <allxn.srn@gmail.com>
2026-07-14 17:41:17 -0400

    cours gestion + security cours

---
+++ src/main/java/fr/servicepedagogique/backend/exception/CodeCoursDejaUtiliseException.java
@@ -0,0 +1,8 @@
+package fr.servicepedagogique.backend.exception;
+
+public class CodeCoursDejaUtiliseException extends RuntimeException {
+
+    public CodeCoursDejaUtiliseException(String message) {
+        super(message);
+    }
+}
